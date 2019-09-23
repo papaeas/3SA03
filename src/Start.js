7a0947ace846a5a4f151d011ff57d8fdf2b63614
papaeas <asd_papae_asas@hotmail.com>
2019-09-23 11:16:23 +0700

    commit 1

---
+++ src/Start.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom'
+import './Start.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './App.css';
