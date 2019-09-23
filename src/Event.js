694422a24db2efa54f8c7477ca6bc06ffbd0aa22
papaeas <asd_papae_asas@hotmail.com>
2019-09-23 11:23:35 +0700

    commit 2

--- src/Event.js
+++ src/Event.js
@@ -2,3 +2,20 @@ import React from 'react';
 import './App.css';
 import App from './App'
 import Check from './Check';
+
+class Event extends React.Component {
+    state = {
+        active: false
+    }
+    activate = () => {
+        this.setState({
+            active: true
+        });
+        if (this.props.number > 3) {
+            this.setState({
+                active: true
+            });
+        }
+        else if (this.state.active === false)
+            this.props.click(this.props.value);
+    }
