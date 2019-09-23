e3223dc27d5a2738f32f78f52a7073c9e2aeeefb
papaeas <asd_papae_asas@hotmail.com>
2019-09-23 11:11:15 +0700

    commit 2

--- src/Check.js
+++ src/Check.js
@@ -3,3 +3,19 @@ import './Check.css';
 import Event from './Event';
 import App from './App'
 
+class Check extends React.Component {
+    render() {
+        //let count_end = this.props.check_count > 5 ? "Game Over" : "Counter : " + this.props.check_count;
+        let count_end = this.props.check_count > 5 ? 'ch2' : '';
+        //let colors = count_end > 5 ? 'area2' : '';
+        let count_ends = this.props.check_count > 5 ? "Game Over" : "Counter : " + this.props.check_count;
+        let test = `ch ${count_end}`
+        return (
+            <div>
+                <div className={test}>
+                    <h1 className={test}>{this.count_end}{count_ends}</h1>
+                </div>
+            </div>
+        )
+    }
+}
