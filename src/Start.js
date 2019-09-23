d9d2f5363e817f0c6e5622780a2fc5659cccbbe6
papaeas <asd_papae_asas@hotmail.com>
2019-09-23 11:17:07 +0700

    commit 2

--- src/Start.js
+++ src/Start.js
@@ -4,3 +4,13 @@ import './Start.css';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './App.css';
+
+function Start() {
+    return (
+      <div className="one">
+        <div className="row">
+          <h1 className="deed">Let's play Now </h1>
+        </div>
+      </div>
+    );
+  }
